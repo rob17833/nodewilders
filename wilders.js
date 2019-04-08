828c96b6bc1f2ed1a1a6df3e79050294e5eadd56
rob17833 <janssensrobert@hotmail.com>
2019-04-08 17:51:04 +0200

    "first commit"

---
+++ wilders.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+
+console.log(chalk.blue('Alessandro'));
+console.log(chalk.green('Geoffray'));
+console.log(chalk.red('Philippe'));
+console.log(chalk.yellow('Bertrand'));
+
